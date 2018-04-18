a0da7a121f98ef776395fa2701a45f800fea3d87
Raymond Chiu <chiur@google.com>
2018-04-17 18:35:20 -0700

    Extract ApkInfo into an xml data file.
    apk-info.xml stores information about the apks.
    
    By default, the supplied apk-info.xml is used.
    --apk-info allows user to specify another file.
    
    Test: none
    Change-Id: Idf93d8ae787c77aa8c66d716056d0ced689b5c68
    Merged-In: Idf93d8ae787c77aa8c66d716056d0ced689b5c68

--- hostside/Android.mk
+++ hostside/Android.mk
@@ -18,6 +18,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_JAVA_RESOURCE_DIRS := res
 
 LOCAL_MODULE := GraphicsBenchmarkHost
 
