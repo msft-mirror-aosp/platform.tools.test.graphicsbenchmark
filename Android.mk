fea8ae7a81bad857787ba4ead995fbee035e2a77
Dan Willemsen <dwillemsen@google.com>
2019-01-24 17:31:49 -0800

    Fix packaging to use intermediate rather than installed files
    Packaging steps should always use intermediate files rather than forcing
    the modules to be installed first. This also lets us change the install
    locations without changing the packaging.
    
    Also switches to soong_zip instead of zip, which creates more
    reproducible results.
    
    Test: m dist gamecore; check out/dist/gamecore.zip
    Change-Id: Icb189c61d4474decb720085a1d92cb90e615b5ed

--- Android.mk
+++ Android.mk
@@ -17,24 +17,14 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 gamecore_dist_host_jar := GameQualificationHelperHost GameQualificationHost truth-prebuilt
-gamecore_dist_host_jar_files := $(foreach m, $(gamecore_dist_host_jar), $(HOST_OUT_JAVA_LIBRARIES)/$(m).jar)
-
 gamecore_dist_test_exe := GameQualificationNativeTestCases
-gamecore_dist_test_exe_files := $(foreach m, $(gamecore_dist_test_exe), $(TARGET_OUT_TESTCASES)/$(m)/$(TARGET_ARCH)/$(m))
-
 gamecore_dist_test_apk := GameQualificationDevice GameQualificationSampleApp GameQualificationJavaTestCases GameQualificationAllocstress
-gamecore_dist_test_apk_files := $(foreach m, $(gamecore_dist_test_apk), $(TARGET_OUT_DATA_APPS)/$(m)/$(m).apk)
 
-gamecore_dist_intermediates := $(call intermediates-dir-for,PACKAGING,gamecore_dist,HOST,COMMON)
-gamecore_dist_dir := $(gamecore_dist_intermediates)/gamecore
-gamecore_dist_zip := $(gamecore_dist_intermediates)/gamecore.zip
-
+tradefed_jars := tradefed tools-common-prebuilt
 tradefed_files := \
-    $(HOST_OUT_JAVA_LIBRARIES)/tradefed.jar \
-    $(HOST_OUT_JAVA_LIBRARIES)/tools-common-prebuilt.jar \
-    $(BUILD_OUT_EXECUTABLES)/tradefed.sh \
-    $(BUILD_OUT_EXECUTABLES)/tradefed_win.bat  \
-    $(BUILD_OUT_EXECUTABLES)/script_help.sh \
+    tools/tradefederation/core/tradefed.sh \
+    tools/tradefederation/core/tradefed_win.bat  \
+    tools/tradefederation/core/script_help.sh \
 
 config_files := \
     $(LOCAL_PATH)/AndroidTest.xml \
@@ -42,26 +32,34 @@ config_files := \
     $(LOCAL_PATH)/dist/run_gamecore.sh \
     $(LOCAL_PATH)/dist/README
 
-gamecore_dist_files := \
-    $(config_files) \
-    $(gamecore_dist_host_jar_files) \
-    $(gamecore_dist_test_apk_files) \
-    $(gamecore_dist_test_exe_files) \
-    $(tradefed_files)
+gamecore_dist_copy_pairs := $(foreach m, $(gamecore_dist_host_jar) $(tradefed_jars), \
+    $(call intermediates-dir-for,JAVA_LIBRARIES,$(m),HOST,COMMON)/javalib.jar:gamecore/bin/$(m).jar)
+gamecore_dist_copy_pairs += $(foreach m, $(gamecore_dist_test_exe), \
+    $(call intermediates-dir-for,NATIVE_TESTS,$(m))/$(m):gamecore/testcases/$(TARGET_ARCH)/$(m))
+gamecore_dist_copy_pairs += $(foreach m, $(gamecore_dist_test_apk), \
+    $(call intermediates-dir-for,APPS,$(m))/package.apk:gamecore/testcases/$(m).apk)
+gamecore_dist_copy_pairs += $(foreach f, $(tradefed_files),$(f):gamecore/bin/$(notdir $(f)))
+gamecore_dist_copy_pairs += $(foreach f, $(config_files),$(f):gamecore/$(notdir $(f)))
 
-# Copy files into appropriate directories and create gamecore.zip
-$(gamecore_dist_zip) : $(gamecore_dist_files)
-	@echo "Package: $@"
-	$(hide) rm -rf $(dir $@) && mkdir -p $(dir $@)/gamecore
-	$(hide) mkdir -p $(dir $@)/gamecore/bin
-	$(hide) mkdir -p $(dir $@)/gamecore/testcases/$(TARGET_ARCH)
-	$(hide) cp -f $(tradefed_files) $(dir $@)/gamecore/bin
-	$(hide) cp -f $(gamecore_dist_host_jar_files) $(dir $@)/gamecore/bin/
-	$(hide) cp -f $(gamecore_dist_test_apk_files) $(dir $@)/gamecore/testcases/
-	$(hide) cp -f $(gamecore_dist_test_exe_files) $(dir $@)/gamecore/testcases/$(TARGET_ARCH)/
-	$(hide) cp -f $(config_files) $(dir $@)/gamecore
-	$(hide) echo $(BUILD_NUMBER_FROM_FILE) > $(dir $@)/gamecore/version.txt
-	$(hide) cd $(dir $@) && zip -q -r $(notdir $@) gamecore
+gamecore_dist_host_jar :=
+gamecore_dist_test_exe :=
+gamecore_dist_test_apk :=
+tradefed_jars :=
+tradefed_files :=
+config_files :=
+
+gamecore_dist_intermediates := $(call intermediates-dir-for,PACKAGING,gamecore_dist,HOST,COMMON)
+gamecore_dist_zip := $(gamecore_dist_intermediates)/gamecore.zip
+$(gamecore_dist_zip) : PRIVATE_COPY_PAIRS := $(gamecore_dist_copy_pairs)
+$(gamecore_dist_zip) : $(SOONG_ZIP) $(foreach p,$(gamecore_dist_copy_pairs),$(call word-colon,1,$(p)))
+	rm -rf $(dir $@) && mkdir -p $(dir $@)/gamecore
+	mkdir -p $(dir $@)/gamecore/bin
+	mkdir -p $(dir $@)/gamecore/testcases/$(TARGET_ARCH)
+	$(foreach p,$(PRIVATE_COPY_PAIRS), \
+	  cp -f $(call word-colon,1,$(p)) $(dir $@)/$(call word-colon,2,$(p)) &&) true
+	echo $(BUILD_NUMBER_FROM_FILE) > $(dir $@)/gamecore/version.txt
+	$(SOONG_ZIP) -o $@ -C $(dir $@) -f $(dir $@)/gamecore/version.txt \
+	  $(foreach p,$(PRIVATE_COPY_PAIRS),-f $(dir $@)/$(call word-colon,2,$(p)))
 
 .PHONY: gamecore
 gamecore: $(gamecore_dist_host_jar) $(gamecore_dist_test_apk)
@@ -73,5 +71,9 @@ gamecore-test: GameQualificationHostTest GameQualificationHelperTest
 gamecore-all: gamecore gamecore-test
 
 $(call dist-for-goals, gamecore, $(gamecore_dist_zip))
+
+gamecore_dist_copy_pairs :=
+gamecore_dist_intermediates :=
+gamecore_dist_zip :=
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
