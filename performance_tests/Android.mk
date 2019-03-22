9ed971e2008e11916019f137719f558d325b55d7
Raymond Chiu <chiur@google.com>
2019-03-22 16:14:53 -0700

    Rename build target with GameCore instead of GameQualification
    Test: tradefed.sh run AndroidTest.xml
    Change-Id: I33e769d854c0ced89c4b265c2b5aa7e4d4556078

--- performance_tests/Android.mk
+++ performance_tests/Android.mk
@@ -1,10 +1,10 @@
-# Copyright 2018, The Android Open Source Project
+# Copyright (C) 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +13,4 @@
 # limitations under the License.
 
 LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SDK_VERSION := 26  # Oreo
-LOCAL_PACKAGE_NAME := GameCoreSampleApp
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
-LOCAL_JNI_SHARED_LIBRARIES := libagq libgamecore_sample
-LOCAL_NDK_STL_VARIANT := c++_shared
-
-include $(BUILD_PACKAGE)
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
